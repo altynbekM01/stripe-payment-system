c922a10cbf6ee3ad129518c0d60ac481f1930edc
altynbekM01 <100331049+altynbekM01@users.noreply.github.com>
2025-05-31 00:03:23 +0600

    Update 0004_create_superuser.py

--- store/migrations/0004_create_superuser.py
+++ store/migrations/0004_create_superuser.py
@@ -1,24 +1,21 @@
-import os
 from django.db import migrations
+from django.contrib.auth import get_user_model
+import os
 
 def create_superuser(apps, schema_editor):
-    User = apps.get_model('auth', 'User')
+    User = get_user_model()
 
     username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
     email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'defaultpassword')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
     if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
+        User.objects.create_superuser(username=username, email=email, password=password)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0003_order_discount_order_tax'),  # проверь актуальную версию в твоём проекте
+        ('store', '0003_order_discount_order_tax'),
     ]
 
     operations = [
