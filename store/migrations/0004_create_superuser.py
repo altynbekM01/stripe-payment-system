25ffd8698ac09d4f047944b79cad2e793ad26d81
altynbekM01 <100331049+altynbekM01@users.noreply.github.com>
2025-05-30 14:50:19 +0500

    Create 0004_create_superuser.py

---
+++ store/migrations/0004_create_superuser.py
@@ -0,0 +1,26 @@
+import os
+from django.db import migrations
+
+def create_superuser(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+
+    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'defaultpassword')
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),  # проверь актуальную версию в твоём проекте
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
